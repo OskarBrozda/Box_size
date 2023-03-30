2d4269b1ba684004d8cee5a254cbda29b4a5ae73
OskarBrozda <oskarbrozdaa@gmail.com>
2023-03-30 15:28:31 +0200

    begin

---
+++ Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLib;
+using P = MyLib.Pudelko;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            Pudelko p1 = new Pudelko(9);
+            Console.WriteLine($"{p1.A}, {p1.B}, {p1.C}, {p1.Measure}");
+        }
+    }
+}
---
+++ box/Pudelko.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using P = MyLib.Pudelko;
+
+namespace MyLib
+{
+    public class Pudelko
+    {
+        private double a { get; set; }
+        public double A
+        {
+            get => Math.Round(a, 3);
+            set
+            {
+                if (value > 10 || value < 0) throw new ArgumentOutOfRangeException();
+                a = value;
+            }
+        }
+
+        private double b { get; set; }
+        public double B
+        {
+            get => Math.Round(b, 3);
+            set
+            {
+                if (value > 10 || value < 0) throw new ArgumentOutOfRangeException();
+                b = value;
+            }
+        }
+
+        private double c { get; set; }
+        public double C
+        {
+            get => Math.Round(c, 3);
+            set
+            {
+                if (value > 10 || value < 0) throw new ArgumentOutOfRangeException();
+                c = value;
+            }
+        }
+
+        public UnitOfMeasure Measure { get; set; }
+          
+        public Pudelko(double a = 10, double b = 10, double c = 10, UnitOfMeasure unit = UnitOfMeasure.meter)
+        {
+            A = a;
+            B = b;
+            C = c;
+            Measure = unit;
+        }
+    }
+}
+
